9f05849615204a74634e4cc22e89c4101bc90d90
Janak2396 <pateljanak1996@gmail.com>
2022-05-18 03:03:59 -0400

    Guidline_update

--- components/Guidelines/index.js
+++ components/Guidelines/index.js
@@ -1,52 +1,30 @@
 import Header from 'components/shared/Header';
-import { ReactComponent as DownArrowbtn } from 'icons/down-arrow.svg';
 import { useEffect, useState } from 'react';
+import getObj from 'utils/fetchfb';
+import ManageCard from './ManageCard';
 
 const Guidelines = () => {
-  const [visible, setVisible] = useState(true);
-  const [getdetail, setDetail] = useState();
-  const data = [
-    { id: 1, name: "John Doe" },
-    { id: 2, name: "Victor Wayne" },
-    { id: 3, name: "Jane Doe" },
-  ];
-// const [data, setData] = useState([]);
-//   async function getData() {
-//     const response = await fetch('https://restcountries.com/v3.1/all');
-//     const myJson = await response.json();
-//     setData(...myJson);
-//   }
-//   useEffect(() => {
-//     getData();
-//   }, []);
- 
-  function getDetailcard(num) {
-    setDetail(num);
-    setVisible(!visible);
-  }
+  const [airpotCodes, setAirportCode] = useState([]);
+  const getAirportCodes = async () => {
+    try {
+      const result = await getObj('/AirportCodes');
+      setAirportCode(Object.values(result));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getAirportCodes();
+  }, []);
   return (
     <div className="guidelines-body">
       <Header />
       <h1>Guidelines</h1>
-      {/* <div className="users">
-        {data.map((user) => (
-          <div className="card">
-            <div className="container">
-              <div className="cnt-code">
-                <h1><b> { user.name } </b></h1>
-              </div>
-              <div className="arrowbtn">
-                <DownArrowbtn onClick={() => getDetailcard(user.id)} />
-              </div>
-            </div>
-            {getdetail === user.id ? visible : !visible && (
-            <div>
-              <h4 className="detail-card">{user.id}</h4>
-            </div>
-            )}
-          </div>
+      <div className="users">
+        {airpotCodes.map((airpotCodes) => (
+          <ManageCard airpotCodes={airpotCodes} />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 };
